a3d7509b453e2534341aa106b6e2e403540d096b
Hugo Henriques Maia Vieira <hugouenf@gmail.com>
2010-05-03 22:11:09 -0300

    Nem lembro o que eu fiz, mas to commitando

--- malhas/malhas.py
+++ malhas/malhas.py
@@ -5,12 +5,29 @@ X=0
 Y=1
 Z=2
 
+def frange(start, end=None, inc=1.0):
+    "A range function, that does accept float increments..."
+
+    if end == None:
+        end = start + 0.0
+        start = 0.0
+
+    L = []
+    while 1:
+        next = start + len(L) * inc
+        if inc > 0 and next >= end:
+            break
+        elif inc < 0 and next <= end:
+            break
+        L.append(next)
+    return L
+
 class Malha(object):
 
     def __init__(self, arquivo):
         arquivo = open(arquivo)
         self.pontos = Malha.ler_dados(arquivo)
-        self.pontos_da_curva = []
+        self.curva_s_t = []
 
     @classmethod
     def ler_dados(self, arquivo):
@@ -35,27 +52,45 @@ class Malha(object):
             matriz.append(aux)
         return matrix(matriz)
 
-    def calcular_pontos_da_curva(self, passo=0.03):
-        self.pontos_da_curva = []
-        s, t = (0,0)
-        while t <= 1:
-            vetor_t = matrix([pow(t,3), pow(t,2), t, 1])
-            vetor_s = matrix([pow(s,3), pow(s,2), s, 1])
+    def calcular_pontos_da_curva(self):
+        self.curva_s_t = []
+        curva_t = []
+        for s in frange(0,1,0.02):
+            for t in frange(0,1,0.03):
+                vetor_s = matrix([pow(s,3), pow(s,2), s, 1])
+                vetor_t = matrix([pow(t,3), pow(t,2), t, 1])
 
-            ponto_x = (vetor_s * self.matriz * self.matriz_pontos(X) * self.matriz.transpose() * vetor_s.transpose())
-            ponto_y = (vetor_s * self.matriz * self.matriz_pontos(Y) * self.matriz.transpose() * vetor_s.transpose())
-            ponto_z = (vetor_s * self.matriz * self.matriz_pontos(Z) * self.matriz.transpose() * vetor_s.transpose())
+                ponto_x = (vetor_s * self.matriz * self.matriz_pontos(X) * self.matriz.transpose() * vetor_s.transpose())
+                ponto_y = (vetor_s * self.matriz * self.matriz_pontos(Y) * self.matriz.transpose() * vetor_s.transpose())
+                ponto_z = (vetor_s * self.matriz * self.matriz_pontos(Z) * self.matriz.transpose() * vetor_s.transpose())
 
-            self.pontos_da_curva.append(array([float(ponto_x), float(ponto_y), float(ponto_z)]))
-            t+=passo
-            s+=passo
+                curva_t.append(array([float(ponto_x), float(ponto_y), float(ponto_z)]))
+            self.curva_s_t.append(curva_t)
 
     def desenha(self):
-        glColor3f(0,0,0)
-        glBegin(GL_LINE_STRIP)
-        for ponto in self.pontos_da_curva:
-            glVertex3f(ponto.item(X), ponto.item(Y), ponto.item(Z))
+        # Desenha pontos
+        glColor3f(1,0,0)
+        glPointSize(3)
+        glBegin(GL_POINTS)
+        for linha in self.pontos:
+            for ponto in linha:
+                glVertex2f(ponto[X],ponto[Y])
         glEnd()
+
+        glColor3f(0.2,0.4,0.6)
+        for linha in self.pontos:
+            for ponto in linha:
+                glBegin(GL_LINES)
+                glVertex2f(ponto[X],ponto[Y])
+                glVertex2f(ponto[X],0)
+                glEnd()
+
+        glColor3f(0,0,1)
+        for linha in self.curva_s_t:
+            glBegin(GL_LINE_STRIP)
+            for ponto in linha:
+                glVertex2f(ponto.item(X), ponto.item(Y))
+            glEnd()
         glFlush()
 
 
